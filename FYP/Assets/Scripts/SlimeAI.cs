59a965507f9536c93c13b16898e4acfa00a48ed9
Alex <79840465+DragOfDojima@users.noreply.github.com>
2024-10-22 16:29:12 +0800

    new ai

--- FYP/Assets/Scripts/SlimeAI.cs
+++ FYP/Assets/Scripts/SlimeAI.cs
@@ -5,20 +5,47 @@ using UnityEngine.AI;
 public class SlimeAI : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public  float speed=1;
+    public float speed;
     public Animator animator;
+    bool playerInSightRange;
+    public float sightRange;
+    public LayerMask whatIsPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetPosition =  Camera.main.transform.position;
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        if (playerInSightRange) ChasePlayer();
+        else Patroling();
+        Vector3 targetPosition = Camera.main.transform.position;
+    }
 
+    private void ChasePlayer()
+    {
+        Vector3 targetPosition = Camera.main.transform.position;
+
+        animator.SetBool("jump", true);
         agent.SetDestination(targetPosition);
-        agent.speed = speed;
+
+        if (animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Slime_idel2jump_baked" || animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Slime_jump2idel_baked" || animator.GetCurrentAnimatorClipInfo(0)[0].clip.name == "Slime_Idel_baked")
+        {
+            agent.speed = 0;
+        }
+        else
+        {
+            agent.speed = speed;
+        }
+
+    }
+
+    private void Patroling()
+    {
+        agent.speed = 0;
+        animator.SetBool("jump", false);
     }
 }
