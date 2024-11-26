0544e681536692b491972306e75aa21985e1c14b
Alex <alexishandsome45@gmail.com>
2024-11-26 22:21:45 +0800

    potion

--- FYP/Assets/Scripts/weapon.cs
+++ FYP/Assets/Scripts/weapon.cs
@@ -7,26 +7,12 @@ using UnityEngine.UIElements;
 
 public class weapon : MonoBehaviour
 {
-    Animator animator;
     [SerializeField] private float WeaponDamage;
-    [SerializeField] private Collider hitbox;
-    [SerializeField] private Rigidbody rb;
     [SerializeField] private float ComboBouns;
     [SerializeField] private int MaxCombo;
     [SerializeField] private GameObject hitEffect;
-    [SerializeField] private Grabbable grabbable;
 
-    [SerializeField] private GameObject[] matObjects;
     private int combo = 0;
-    public bool NotWeapon=false;
-    bool isIdel=true;
-
-    void Start()
-    {
-        animator = GetComponent<Animator>();
-
-    }
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -44,36 +30,11 @@ public class weapon : MonoBehaviour
             }
         }
     }
-    bool isgrabing;
-    bool setup;
-    bool setup2;
     float timer=3;
     Vector3 lastPosition = Vector3.zero;
     private void Update()
     {
-        if(grabbable.SelectingPoints.Count > 0)
-        {
-            isIdel = false;
-            isgrabing = true;
-            if (!setup)
-            {
-                setup = true;
-                Setup();
-            }
-        }
-        else
-        {
-            if (isgrabing == true)
-            {
-                Invoke("setIdel",3f);
-            }
-            isgrabing= false;
-            if (setup&&!setup2)
-            {
-                setup2 = true;
-                Setup();
-            }
-        }
+        
         //Debug.Log("c=" + combo);
         if (combo > 0)
         {
@@ -85,27 +46,8 @@ public class weapon : MonoBehaviour
             combo=0;
             timer = 3;
         }
-        
-        
-        
     }
 
-    public bool getIdel()
-    {
-        return isIdel;
-    }
-    void setIdel()
-    {
-        isIdel = true;
-    }
-
-    void Setup()
-    {
-        animator.enabled = false;
-        rb.isKinematic = false;
-        rb.useGravity = true;
-        hitbox.isTrigger = false;
-    }
     float speed;
     private void FixedUpdate()
     {
@@ -114,18 +56,4 @@ public class weapon : MonoBehaviour
         
     }
 
-    public bool getIsGrabing()
-    {
-        return isgrabing;
-    }
-
-    public GameObject[] getMatObjects()
-    {
-        return matObjects;
-    }
-
-    public bool getNotWeapon()
-    {
-        return NotWeapon;
-    }
 }
