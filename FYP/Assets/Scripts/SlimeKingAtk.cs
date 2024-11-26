d29c951e50d5ae83c5f4b0c669bb08c293601b18
Alex <79840465+DragOfDojima@users.noreply.github.com>
2024-11-26 16:58:42 +0800

    v

--- FYP/Assets/Scripts/SlimeKingAtk.cs
+++ FYP/Assets/Scripts/SlimeKingAtk.cs
@@ -6,19 +6,12 @@ public class SlimeKingAtk : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody rb;
-    [SerializeField] float dealDanage;
-    [SerializeField] LayerMask enemy;
-    bool igcol=false;
-    public void setDamage(float d)
-    {
-        dealDanage = d;
-    }
     void Start()
     {
         rb= GetComponent<Rigidbody>();
         rb.AddForce(transform.forward   * 5);
         Destroy(gameObject, 5f);
-        Invoke("spawn",0.2f);
+    
     }
 
     // Update is called once per frame
@@ -26,21 +19,5 @@ public class SlimeKingAtk : MonoBehaviour
     {
         
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log(other.gameObject.name);
-        if (other.tag == "Player")
-        {
-            other.GetComponent<ToPlayer>().getplayer().setHp(-dealDanage);
-            Destroy(gameObject);
-        }
-        else if(other.gameObject.layer!=enemy&&igcol==true)
-            Destroy(gameObject);
-    }
-
-    void spawn()
-    {
-        igcol=true;
-    }
+    
 }
