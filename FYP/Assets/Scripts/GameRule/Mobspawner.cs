18763ce4ebaebb9756948c05d425291f6e3ebc4d
Alex <79840465+DragOfDojima@users.noreply.github.com>
2025-04-15 15:50:55 +0800

    up

--- FYP/Assets/Scripts/GameRule/Mobspawner.cs
+++ FYP/Assets/Scripts/GameRule/Mobspawner.cs
@@ -95,6 +95,7 @@ public class Mobspawner : MonoBehaviour
     public void endedWave(int max)
     {
         wave.setRest(true);
+        GameObject.Find("Player").GetComponent<Player>().increaseHp(30);
         maxWave = max;
         endWave = true;
         StartCoroutine(wait());
