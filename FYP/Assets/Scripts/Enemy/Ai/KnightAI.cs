c38db642294e42833e32110d231adb39034a4565
Alex <79840465+DragOfDojima@users.noreply.github.com>
2025-04-15 15:15:53 +0800

    up

--- FYP/Assets/Scripts/Enemy/Ai/KnightAI.cs
+++ FYP/Assets/Scripts/Enemy/Ai/KnightAI.cs
@@ -88,6 +88,8 @@ public class KnightAI : MonoBehaviour
         if(agent.isActiveAndEnabled)
         agent.SetDestination(targetPosition);
         playerInCloseRange = Physics.CheckSphere(transform.position, 1.4f, whatIsPlayer);
+        if(animator.GetCurrentAnimatorStateInfo(0).IsName("slash1")|| animator.GetCurrentAnimatorStateInfo(0).IsName("slash2"))
+            return;
         if (playerInCloseRange && animator.enabled == true)
         {
             enemyWeapon.hitedShield=false;
