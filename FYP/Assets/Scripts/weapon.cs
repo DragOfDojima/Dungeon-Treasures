30303d5201cc64fd59f2cb8ff412144dc479faed
Alex <79840465+DragOfDojima@users.noreply.github.com>
2025-04-15 14:41:01 +0800

    bug

--- FYP/Assets/Scripts/weapon.cs
+++ FYP/Assets/Scripts/weapon.cs
@@ -31,7 +31,7 @@ public class weapon : MonoBehaviour
         
         if (other.tag == "hitable")
         {
-            if (speed > 3.5)
+            if (speed > 2)
             {
                 var hitPoint = other.ClosestPoint(transform.position);
                 Instantiate(hitEffect, hitPoint, Quaternion.identity);
